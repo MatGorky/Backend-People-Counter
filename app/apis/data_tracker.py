45bee23495dc420c00d72070e83e91115aa4ccdd
MatGorky <mouragorchinsky@gmail.com>
2025-05-18 11:20:14 -0300

    fix: Fixes auth decorator

--- app/apis/data_tracker.py
+++ app/apis/data_tracker.py
@@ -1,12 +1,38 @@
-from flask_restx import Resource, Namespace
+from flask_restx import Resource, Namespace, fields, marshal_with, abort
 from app.models.data_tracker import DataTracker
 from app.utils.auth import require_auth
 from sqlalchemy import func, select, Date, cast, Time, DateTime
+from datetime import datetime, timedelta
 from app import db
 
-api = Namespace('Data Tracker', description='Endpoints to get unhandled mqtt subscriptions')
+api = Namespace(
+    "data-tracker", description="Endpoints to get unhandled mqtt subscriptions"
+)
 
-@api.route('/tracker')
+
+data_by_month = api.model(
+    "Data grouped by day",
+    {
+        "day": fields.Date(
+            dt_format="iso8601", description="The date(day) of the aggregate"
+        ),
+        "count": fields.Integer(description="The count of tests for that day"),
+    },
+)
+
+data_by_day = api.model(
+    "Data grouped by hour",
+    {
+        "time": fields.DateTime(
+            dt_format="iso8601",
+            description="The time(hour of the day) of the aggregate",
+        ),
+        "count": fields.Integer(description="The count of passages for that hour"),
+    },
+)
+
+
+@api.route("/tracker")
 class DataTrackerResource(Resource):
     @require_auth
     def get(self):
@@ -15,11 +41,49 @@ class DataTrackerResource(Resource):
         ).all()
         return data
 
-    
-@api.route('/tracker/<string:topic>')
+
+@api.route("/daily/<string:date>")
+class DataTrackerResourceByDay(Resource):
+    @api.doc(params={"date": "Query date in YYYY-MM-DD format"})
+    @api.marshal_with(data_by_day, envelope="data", code=200)
+    @api.response(400, "Invalid date format")
+    @require_auth()
+    def get(self, date):
+        try:
+
+            date = datetime.strptime(date, "%Y-%m-%d")
+            query = (
+                select(
+                    func.date_part("hour", DataTracker.register_time).label("hour"),
+                    func.count(DataTracker.id).label("count"),
+                )
+                .filter(func.date(DataTracker.register_time) == date)
+                .group_by(func.date_part("hour", DataTracker.register_time))
+            )
+
+            result = db.session.execute(query).all()
+            data = [
+                {
+                    "time": datetime(date.year, date.month, date.day, int(r.hour)),
+                    "count": r.count,
+                }
+                for r in result
+            ]
+            print(data)
+            return data
+
+        except ValueError:
+            return {"error": "Invalid date format. Please use YYYY-MM-DD."}, 400
+
+
+@api.route("/tracker/<string:topic>")
 class DataTrackerResourceByTopic(Resource):
-    def get(self,topic):
-        data = db.session.execute(
-            select(DataTracker.id, DataTracker.topic, DataTracker.payload)
-        ).filter_by(topic=topic).all()
+    def get(self, topic):
+        data = (
+            db.session.execute(
+                select(DataTracker.id, DataTracker.topic, DataTracker.payload)
+            )
+            .filter_by(topic=topic)
+            .all()
+        )
         return data
--- app/utils/auth.py
+++ app/utils/auth.py
@@ -1,10 +1,10 @@
 import jwt
 from functools import wraps
 from flask import request, jsonify, g
+from flask_restx import abort
 import os
 
 SUPABASE_JWT_SECRET = os.getenv("SUPABASE_JWT_SECRET")
-
 
 
 def require_auth(roles=None):
@@ -13,28 +13,35 @@ def require_auth(roles=None):
         def decorated_function(*args, **kwargs):
             auth_header = request.headers.get("Authorization", "")
             if not auth_header.startswith("Bearer "):
-                return jsonify({"error": "Authorization header missing or malformed"}), 401
+                abort(401, "Authorization header missing or malformed")
 
             token = auth_header.split(" ")[1]
 
             try:
-                payload = jwt.decode(token, SUPABASE_JWT_SECRET, algorithms=["HS256"])
+                payload = jwt.decode(
+                    token,
+                    SUPABASE_JWT_SECRET,
+                    algorithms=["HS256"],
+                    audience="authenticated",
+                )
                 user_role = payload.get("role")
-                
+
                 if roles and user_role not in roles:
-                    return jsonify({"error": "Insufficient permissions"}), 403
+                    abort(403, "Insufficient permissions")
 
                 g.user = {
                     "id": payload.get("sub"),
                     "email": payload.get("email"),
-                    "role": user_role
+                    "role": user_role,
                 }
 
             except jwt.ExpiredSignatureError:
-                return jsonify({"error": "Token expired"}), 401
+                abort(401, "Token expired")
             except jwt.InvalidTokenError:
-                return jsonify({"error": "Invalid token"}), 401
+                abort(401, "Invalid token")
 
             return f(*args, **kwargs)
+
         return decorated_function
+
     return decorator
